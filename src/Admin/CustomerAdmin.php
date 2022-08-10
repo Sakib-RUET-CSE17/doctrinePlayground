fb316cd588806ad7fb65d679672d4e38fb2ef529
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-08-10 17:52:20 +0600

    init sonata

---
+++ src/Admin/CustomerAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+final class CustomerAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('id')
+            ->add('name')
+            ->add('phone');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('id')
+            ->add('name')
+            ->add('phone')
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('name')
+            ->add('phone');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('id')
+            ->add('name')
+            ->add('phone');
+    }
+}
---
+++ src/Admin/ProductAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+final class ProductAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('id')
+            ->add('name')
+            ->add('price')
+            ->add('stock');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('id')
+            ->add('name')
+            ->add('price')
+            ->add('stock')
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('name')
+            ->add('price')
+            ->add('stock');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('id')
+            ->add('name')
+            ->add('price')
+            ->add('stock');
+    }
+}
